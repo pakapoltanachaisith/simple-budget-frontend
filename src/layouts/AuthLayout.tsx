f7f607d3462ffb59cb98c509b8d91f73f00439dc
Pakapol Tanachaisith <pakapol.tanachaisith@gmail.com>
2026-08-26 22:35:27 +0700

    Fix overflow issue in Auth layout

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router";
 
 export default function AuthLayout() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-stone-50-50">
-      <main className="min-w-100">
+    <div className="h-screen w-screen overflow-y-auto overflow-x-hidden flex items-center justify-center bg-stone-50-50">
+      <main className="min-w-100 max-w-160">
         <Outlet />
       </main>
     </div>
