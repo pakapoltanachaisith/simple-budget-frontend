dbf79108df7e14231b3df67c0336c3e50b4bee3b
Pakapol Tanachaisith <pakapol.tanachaisith@gmail.com>
2026-08-26 14:57:16 +0700

    Install Tailwind CSS

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 export default function HomePage() {
   return (
     <div>
-      Home Page
+      <h1 className="text-4xl text-blue-500">Home Page</h1>
       <Link to="/login">Go to Login Page</Link>
       <Link to="/register">Go to Register Page</Link>
     </div>
